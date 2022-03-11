47851538fa9b6ed60d19d3263dec434f84b571c6
Muhammad Taimoor <32198560+Taimoor10@users.noreply.github.com>
2022-03-11 10:43:59 +0100

    Update qrCode.js

--- Thesis/Backend/qrCode.js
+++ Thesis/Backend/qrCode.js
@@ -4,8 +4,6 @@ module.exports = function QRcodeGenerator(web3, QRcode){
 this.generatesQRcode = async(msg) => {
 
     try{
-        // console.log(await QRcode.toString([{data: web3.utils.hexToBytes(web3.utils.toHex(msg)),mode: 'byte'}],
-        //                                  {type: 'utf8', errorCorrectionLevel :'H'}))
         await QRcode.toFile('./public/images/image.png', [{data: web3.utils.hexToBytes(web3.utils.toHex(msg)), mode:'byte'}],
                 {type: 'png', errorCorrectionLevel: 'H'}, 
                 function(err){
